06a66e8e694c332567d21dd7f8b2095270525575
Emil Andersson <vaccuum@gmail.com>
2015-01-24 22:17:55 +0100

    switchLayer functionality

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -55,38 +55,77 @@ function switchScene(sceneID) {
 
     // assuming there's only two scenes for now, 1 day, 2 night
     dayOrNight = (dayOrNight === 'day' ? 'night' : 'day');
+    console.log('It is now ' + dayOrNight);
 }
 
 // up or down
 function switchLayer(direction) {
-    var nextLayerID = dayOrNight + '-',
-    previousLayerID = dayOrNight + '-';
+    var newBackground = dayOrNight + '-',
+        newMiddleground = dayOrNight + '-',
+        newForeground = dayOrNight + '-',
+        currentBackground = dayOrNight + '-' + (currentLayer - 1),
+        currentMiddleground = dayOrNight + '-' + currentLayer,
+        currentForeground = dayOrNight + '-' + (currentLayer + 1),
+        container;
 
     // If you're going up, and you're not already in the top layer
-    if (direction === 'up' && currentLayer > 1) {
-        // The ID of the layer above
-        nextLayerID += currentLayer - 2;
-        previousLayerID += currentLayer + 1;
-        console.log('nextLayerID  ' + nextLayerID);
-        console.log('previousLayerID  ' + previousLayerID);
+    if (direction === 'up' && currentLayer > 3) {
+        newBackground += (currentLayer - 2);
+        newMiddleground = currentBackground;
+        newForeground = currentMiddleground;
 
-        // display the layer above you
-        document.getElementById(nextLayerID).classList.remove('hidden');
+        // display the new background
+        document.getElementById(newBackground).classList.remove('hidden');
 
         // play zoom animation
 
 
-        // hide old bottom layer
-        document.getElementById(previousLayerID).classList.add('hidden');
+        // hide old foreground
+        document.getElementById(currentForeground).classList.add('hidden');
+
+        // Update current layer
+        currentLayer -= 1;
+
+    } else if (direction === 'down' && currentLayer < 5) {
+        newBackground = currentMiddleground;
+        newMiddleground = currentForeground;
+        newForeground += (currentLayer + 2);
+
+        // display the new foreground
+        document.getElementById(newForeground).classList.remove('hidden');
+
+        // play zoom animation
         
-        // update normal, half and twice classes
 
-    } else if (direction === 'down' /*and you are not already all the way down*/) {
-        // display one more layer
-        // play zoom animation
-        // hide old bottom layer
-        // update classes
-    }
+        // hide old background
+        document.getElementById(currentBackground).classList.add('hidden');
+
+        // Updte current layer
+        currentLayer += 1;
+    } else { return console.log("You can't move " + direction + ", you've reached the edge."); }
+
+    // console.log('currentBackground ' + currentBackground);
+    // console.log('currentMiddleground ' + currentMiddleground);
+    // console.log('currentForeground ' + currentForeground);
+    // console.log('newBackground ' + newBackground);
+    // console.log('newMiddleground ' + newMiddleground);
+    // console.log('newForeground ' + newForeground);
+
+    // update normal, half and twice classes
+    document.getElementById(currentBackground).classList.remove('layer--half');
+    document.getElementById(currentMiddleground).classList.remove('layer--normal');
+    document.getElementById(currentForeground).classList.remove('layer--twice');
+    document.getElementById(newBackground).classList.add('layer--half');
+    document.getElementById(newMiddleground).classList.add('layer--normal');
+    document.getElementById(newForeground).classList.add('layer--twice');
 
     // Update moveable layers to currently displayed ones
+    if (dayOrNight === 'day') {container = document.querySelector('#crashSiteDay');};
+    if (dayOrNight === 'night') {container = document.querySelector('#crashSiteNight');};
+    var layers = [
+        document.getElementById(newBackground),
+        document.getElementById(newMiddleground),
+        document.getElementById(newForeground)
+    ];
+    makeSceneMovable(container, layers);
 }
