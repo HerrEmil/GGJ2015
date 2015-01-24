f3525d3cdffe7c21d4a4eab0f4300ff200c6186c
Björn Söderqvist <cybear@gmail.com>
2015-01-24 15:46:56 +0100

    Introducing makeSceneMovable function

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,30 +1,25 @@
-var full = document.querySelector('.full');
-var layers = [
-    document.querySelector('.layer--half'),
-    document.querySelector('.layer--normal'),
-    document.querySelector('.layer--twice')
-];
+function makeSceneMovable(container, layers) {
+    var startX;
+    var currentPosition = 0;
 
-var startX;
-var currentPosition = 0;
-
-function drag (e) {
-    var diffX = (e.offsetX - startX);
-    for(var i=0; i<3; i++){
-        layers[i].style.backgroundPosition = diffX * 0.5 + 'px';
-        currentPosition = diffX;
+    function drag (e) {
+        var diffX = (e.offsetX - startX);
+        for(var i=0; i<3; i++){
+            layers[i].style.backgroundPosition = diffX * 0.5 + 'px';
+            currentPosition = diffX;
+        }
     }
-}
 
-full.addEventListener('mousedown', function(e){
-    startX = e.offsetX - currentPosition;
-    full.addEventListener('mousemove', drag);
-    full.addEventListener('mouseup', function(){
-        full.removeEventListener('mousemove', drag);
-    })
-});
+    container.addEventListener('mousedown', function(e){
+        startX = e.offsetX - currentPosition;
+        container.addEventListener('mousemove', drag);
+        container.addEventListener('mouseup', function(){
+            container.removeEventListener('mousemove', drag);
+        })
+    });
 
-// Start positions
-for(var i=0; i<3; i++){
-    layers[i].style.backgroundPosition = '0px';
+    // Start positions
+    for(var i=0; i<3; i++){
+        layers[i].style.backgroundPosition = '0px';
+    }    
 }
