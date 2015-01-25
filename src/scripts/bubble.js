93018c77681f4a870edfc03fe2a8ad4ab25b71f8
Björn Söderqvist <cybear@gmail.com>
2015-01-25 14:28:59 +0100

    Bubble code

--- src/scripts/bubble.js
+++ src/scripts/bubble.js
@@ -8,4 +8,11 @@ function bubble(id) {
 function bubbleOnce(id){
     var url = 'pratbubblar/mgj_pratbubblar-'+id+'.svg';
     console.log(url);
+    var el = document.createElement('div');
+    el.className = 'bubble';
+    el.setAttribute('style', 'background-image: url('+url+')');;
+    el.addEventListener('click', function(){
+        el.style.display = 'none';
+    });
+    document.body.appendChild(el);
 }
