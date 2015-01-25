7e7d83e63f04721009ac0415a2b02761c9b1cceb
Björn Söderqvist <cybear@gmail.com>
2015-01-25 12:57:46 +0100

    Simplify setupDay

--- src/scripts/setupDay.js
+++ src/scripts/setupDay.js
@@ -1,65 +1,31 @@
+function clickHandler(id){
+    console.log('clickHandler', id);
+}
+
+function handleClicks(f, ids) {
+    ids.forEach(function(id){
+        var querySelector = '#' + id;
+        f.select( querySelector ).click(function() {
+            clickHandler(id);
+        });
+    });
+}
 function setupDay(container) {
     // Load day images
     importSVG('images/mgj-03.svg', '#day-2', function(f) {
-        f.select('#no_x5F_snake').click(function(){
-            alert('snaaake');
-        });
-        f.select('#peter_2_').click(function(){
-            alert('Peter cactus 2');
-        });
-        f.select('#peter').click(function(){
-            alert('Peter cactus');
-        });
+        handleClicks(f, ['no_x5F_snake', 'peter_2_', 'peter']);
     });
     importSVG('images/mgj-04.svg', '#day-3', function(f) {
-        f.select('#peter_17_').click(function(){
-            alert('Peter Cactus');
-        });
-        f.select('#dead_x5F_tree_1_').click(function(){
-            alert('Dead Tree');
-        });
-        f.select('#well').click(function(){
-            alert('Well');
-        });
-        f.select('#wolf_1_').click(function(){
-            alert('Wolf');
-        });
+        handleClicks(f, ['peter_17_', 'dead_x5F_tree_1_', 'well','wolf_1_']);
     });
     importSVG('images/mgj-05.svg', '#day-4', function(f) {
-        f.select('#peter_4_').click(function(){
-            alert('Peter Cactus');
-        });
-        f.select('#peter_3_').click(function(){
-            alert('Peter Cactus');
-        });
-        f.select('#peter_5_').click(function(){
-            alert('Peter Cactus');
-        });
-        f.select('#dead_x5F_tree_3_').click(function(){
-            alert('Dead Tree');
-        });
+        handleClicks(f, ['peter_4_', 'peter_3_', 'peter_5_','dead_x5F_tree_3_']);
     });
     importSVG('images/mgj-06.svg', '#day-5', function(f) {
-        f.select('#peter_6_').click(function(){
-            alert('Peter Cactus');
-        });
-        f.select('#tent').click(function(){
-            alert('Tent');
-        });
-        f.select('#snake').click(function(){
-            alert('Snake');
-        });
+        handleClicks(f, ['peter_6_', 'tent', 'snake']);
     });
     importSVG('images/mgj-07.svg', '#day-6', function(f) {
-        f.select('#peter_16_').click(function(){
-            alert('Peter Cactus');
-        });
-        f.select('#peter_9_').click(function(){
-            alert('Peter Cactus');
-        });
-        f.select('#peter_1_').click(function(){
-            alert('Peter Cactus');
-        });
+       handleClicks(f, ['peter_16_', 'peter_9_', 'peter_1_']); 
     });
 
     // Make visible day layers moveable
