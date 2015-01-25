42e3932892447e43d0ab2a71acd78d32428af6da
Emil Andersson <vaccuum@gmail.com>
2015-01-25 12:42:57 +0100

    Updated day click events, new images

--- src/scripts/setupDay.js
+++ src/scripts/setupDay.js
@@ -1,40 +1,64 @@
 function setupDay(container) {
     // Load day images
     importSVG('images/mgj-03.svg', '#day-2', function(f) {
-        f.select('#snake_1_').click(function(){
+        f.select('#no_x5F_snake').click(function(){
             alert('snaaake');
+        });
+        f.select('#peter_2_').click(function(){
+            alert('Peter cactus 2');
         });
         f.select('#peter').click(function(){
             alert('Peter cactus');
         });
     });
     importSVG('images/mgj-04.svg', '#day-3', function(f) {
-        f.select('#tent_1_').click(function(){
-            alert('tent 1');
+        f.select('#peter_17_').click(function(){
+            alert('Peter Cactus');
+        });
+        f.select('#dead_x5F_tree_1_').click(function(){
+            alert('Dead Tree');
+        });
+        f.select('#well').click(function(){
+            alert('Well');
+        });
+        f.select('#wolf_1_').click(function(){
+            alert('Wolf');
         });
     });
     importSVG('images/mgj-05.svg', '#day-4', function(f) {
-        f.select('#well_2_').click(function(){
-            alert('well');
+        f.select('#peter_4_').click(function(){
+            alert('Peter Cactus');
+        });
+        f.select('#peter_3_').click(function(){
+            alert('Peter Cactus');
+        });
+        f.select('#peter_5_').click(function(){
+            alert('Peter Cactus');
+        });
+        f.select('#dead_x5F_tree_3_').click(function(){
+            alert('Dead Tree');
         });
     });
     importSVG('images/mgj-06.svg', '#day-5', function(f) {
-        f.select('#peter_1_').click(function(){
-            alert('peter 1');
+        f.select('#peter_6_').click(function(){
+            alert('Peter Cactus');
         });
-        f.select('#dead_x5F_tree').click(function(){
-            alert('dead tree');
+        f.select('#tent').click(function(){
+            alert('Tent');
+        });
+        f.select('#snake').click(function(){
+            alert('Snake');
         });
     });
     importSVG('images/mgj-07.svg', '#day-6', function(f) {
-        f.select('#snake').click(function(){
-            alert('snake');
+        f.select('#peter_16_').click(function(){
+            alert('Peter Cactus');
         });
-        f.select('#tent').click(function(){
-            alert('tent');
+        f.select('#peter_9_').click(function(){
+            alert('Peter Cactus');
         });
-        f.select('#bag').click(function(){
-            alert('bag');
+        f.select('#peter_1_').click(function(){
+            alert('Peter Cactus');
         });
     });
 
